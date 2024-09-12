8a62ec896267db3068c81b99cd7b7d222162d81b
souchiyan <souchiyan98@gmail.com>
2024-09-12 17:13:43 +0900

    7-2kadai4

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="ja">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
     <meta charset="UTF-8">
@@ -19,6 +19,9 @@
 
 
             @endforeach
+        </div>
+        <div class='paginate'>
+            {{ $posts->links() }}
         </div>
 
 
